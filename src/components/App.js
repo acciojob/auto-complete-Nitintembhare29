3ab125da741bf966ffa9a82a09ba730eecbe4a56
Nitin Tembhare <nitintembhare340@gmail.com>
2023-06-24 14:35:48 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
-import React from "react";
+import React,{useEffect, useState} from "react";
 import './../styles/App.css';
 
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 const App = () => {
+
+  const[item, setItem] = useState("")
+  const[filter, setFilter] = useState([])
+
+  useEffect(()=>{
+    let arr = fruits.filter(fruit=>{
+      return (fruit.includes(item))
+    })
+    setFilter(arr)
+  },[item])
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Search item</h1>
+        <input type="text" onChange={e=>setItem(e.target.value)}/>
+
+       {
+       item &&  (
+        <ul>
+          {
+            filter.map(item=>(
+              <li>{item}</li>
+            ))
+          }
+        </ul>
+       )
+        }
     </div>
   )
 }
